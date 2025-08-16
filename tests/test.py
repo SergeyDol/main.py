19c17b5c9851e5ae12f7df3c6a3c99fad9d879e5
Сергей Долотов <dolotovsergey04@mail.ru>
2025-08-16 18:35:23 +0700

    Fixing past homework

---
+++ tests/test.py
@@ -0,0 +1,18 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Маскирует номер карты в формате XXXX XX** **** XXXX."""
+    if not card_number.isdigit() or len(card_number) != 16:
+        raise ValueError("Номер карты должен состоять из 16 цифр")
+
+    masked_part = "** ****"
+    return f"{card_number[:4]} {card_number[4:6]}{masked_part} {card_number[-4:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Маскирует номер счёта в формате **XXXX."""
+    if not account_number.isdigit() or len(account_number) < 4:
+        raise ValueError("Номер счёта должен содержать минимум 4 цифры")
+
+    return f"**{account_number[-4:]}"
+
+print(get_mask_card_number("7000792289606361"))
+print(get_mask_account("73654108430135874305"))
